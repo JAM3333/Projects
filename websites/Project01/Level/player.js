7b6698b5b6998a7973b3ce70526429c70f70b898
JAM <jonas.muxel@student.htldornbirn.at>
2023-03-27 16:35:19 +0200

    Update player.js

--- websites/Project01/Level/player.js
+++ websites/Project01/Level/player.js
@@ -1,1 +1,23 @@
 let player;
+let playerStyle;
+let playerPos = [window.width/2, window.height/2] 
+
+function createPlayer{
+    player = document.createElement("div"); playerStyle = player.style;
+    playerStyle.width = "50px";
+    playerStyle.height = "50px";
+    playerStyle.position = "absolute";
+    playerStyle.top = playerPos.X
+}
+
+
+
+const gameSetup = function {
+    createPlayer();
+
+}
+
+const masterUpdate = function {
+
+    window.requestAnimationFrame(masterUpdate);
+}
