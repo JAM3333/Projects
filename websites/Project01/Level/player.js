66788c2726d0c3f12f871b499d5636bf888b4bbe
JAM <jonas.muxel@student.htldornbirn.at>
2023-03-27 18:35:43 +0200

    doin some work

--- websites/Project01/Level/player.js
+++ websites/Project01/Level/player.js
@@ -1,23 +1,54 @@
 let player;
 let playerStyle;
-let playerPos = [window.width/2, window.height/2] 
 
-function createPlayer{
-    player = document.createElement("div"); playerStyle = player.style;
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+let input = [0,0,0,0];
+let keyCodes = ["KeyW","KeyA","KeyS","KeyD"];
+let walkspeed = 5;
+let playerPos = [document.documentElement.clientWidth/2, document.documentElement.clientHeight/2] ;
+
+addEventListener("keydown",function(inputCode){
+    for (let i = 0; i < keyCodes.length; i++){
+        console.log(input)
+        if (inputCode.code === keyCodes[i]){
+            input[i] = walkspeed;
+        }
+    }
+})
+
+addEventListener("keyup",function(inputCode){
+    for (let i = 0; i < keyCodes.length; i++){
+        if (inputCode.code === keyCodes[i]){
+            input[i] = 0;
+        }
+    }
+})
+
+
+function createPlayer(){
+    player = document.createElement("div"); 
+    playerStyle = player.style;
+    
     playerStyle.width = "50px";
     playerStyle.height = "50px";
     playerStyle.position = "absolute";
-    playerStyle.top = playerPos.X
+    playerStyle.backgroundColor = "blue";
+    playerStyle.left = playerPos[0]+"px";
+    playerStyle.top = playerPos[1]+"px";
+    document.body.appendChild(player);
 }
 
 
 
-const gameSetup = function {
+
+const gameSetup = function(){
     createPlayer();
-
+    masterUpdate();
 }
+gameSetup();
 
-const masterUpdate = function {
-
-    window.requestAnimationFrame(masterUpdate);
+function masterUpdate(){
+    requestAnimationFrame(masterUpdate);
 }
